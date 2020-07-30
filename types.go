37e36e2f1583a03b86d7086b1f533b7f07a786d9
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-30 17:05:27 +0100

    move types to separate file

---
+++ types.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/bruno-anjos/archimedes/api"
+)
+
+type (
+	typeInstancesMapKey   = string
+	typeInstancesMapValue = *Instance
+
+	Service struct {
+		Id           string
+		ServiceDTO   *api.ServiceDTO
+		InstancesMap *sync.Map
+	}
+)
+
+type Instance struct {
+	Id          string
+	Service     *Service
+	Ip          string
+	InstanceDTO *api.InstanceDTO
+}
+
+type PairServiceIdStatus struct {
+	ServiceId string
+	IsUp      bool
+	Mutex     *sync.Mutex
+}
