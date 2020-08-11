9322df916703dd10927d754f55135679edccf3d9
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-11 10:29:39 +0100

    add discover and neighbor DTOs

--- api/requests.go
+++ api/requests.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
 )
 
 type ServiceDTO struct {
@@ -12,4 +13,22 @@ type InstanceDTO struct {
 	Static          bool
 	PortTranslation nat.PortMap `json:"port_translation"`
 	Local           bool
+}
+
+type DiscoverDTO struct {
+	MessageId     uuid.UUID
+	Host          string
+	ServicesIds   []string
+	Services      []*ServiceDTO
+	Instances     map[string][]string
+	InstancesDTOs map[string]*InstanceDTO
+	ServicesHash  []byte
+	Hops          int
+	MaxHops       int
+	Timestamp     string
+}
+
+type NeighborDTO struct {
+	ArchimedesId string
+	Addr         string
 }
