2243114a8ae050129c84afe6bc7e71279da4a86f
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-13 10:43:03 +0100

    add service method

--- api/types.go
+++ api/types.go
@@ -11,19 +11,52 @@ type (
 	TypeInstancesMapValue = *Instance
 
 	Service struct {
-		Id           string
-		Ports        nat.PortSet
-		InstancesMap *sync.Map
+		Id              string
+		Ports           nat.PortSet
+		Version         int
 	}
 )
 
+func (s *Service) ToTransfarable() *Service {
+	return &Service{
+		Id:      s.Id,
+		Ports:   s.Ports,
+		Version: s.Version,
+	}
+}
+
 type Instance struct {
 	Id              string
-	Service         *Service
+	ServiceId       string
 	Ip              string
 	PortTranslation nat.PortMap
 	Initialized     bool
+	Static          bool
 	Local           bool
+}
+
+func (i *Instance) ToTransfarable() *Instance {
+	return &Instance{
+		Id:              i.Id,
+		ServiceId:       i.ServiceId,
+		Ip:              "",
+		PortTranslation: i.PortTranslation,
+		Initialized:     i.Initialized,
+		Static:          i.Static,
+		Local:           false,
+	}
+}
+
+func FromTransfarableInstance(i *Instance, originIp string) *Instance {
+	return &Instance{
+		Id:              i.Id,
+		ServiceId:       i.ServiceId,
+		Ip:              originIp,
+		PortTranslation: i.PortTranslation,
+		Initialized:     i.Initialized,
+		Static:          i.Static,
+		Local:           false,
+	}
 }
 
 type PairServiceIdStatus struct {
